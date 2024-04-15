b0f3f75ea78b2c5a72a0fb7fc1d72b7c31531ad9
Jufrench <ju.french@gmail.com>
2024-04-14 20:58:53 -0500

    Use svg as component while I figure out why icon component isn't rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,6 +41,20 @@ const Header = () => {
   )
 };
 
+const LogoSvg = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none"  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-square-half">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M12 4v16" />
+      <path d="M3 3m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z" />
+      <path d="M12 13l7.5 -7.5" />
+      <path d="M12 18l8 -8" />
+      <path d="M15 20l5 -5" />
+      <path d="M12 8l4 -4" />
+    </svg>
+  )
+}
+
 const HeaderMobile = () => {
   const [opened, { open, close }] = useDisclosure(false);
   const navText = ['Home', 'About', 'Project', 'Contact'];
@@ -62,8 +76,11 @@ const HeaderMobile = () => {
   return (
     <header>
       <Group p="md" style={{width: "90%", margin: "0 auto"}} justify="space-between">
-        <Avatar variant="transparent" color="#00adad" radius="0" style={{color: "#00adad"}}>
+        {/* <Avatar variant="transparent" color="#00adad" radius="0" style={{color: "#00adad"}}>
           <IconSquareHalf size="xs" strokeWidth={2} stroke="#00adad" />
+        </Avatar> */}
+        <Avatar variant="transparent" color="#00adad" radius="0" style={{color: "#00adad"}}>
+          <LogoSvg />
         </Avatar>
         <ActionIcon variant="outline" color="gray" size="xl" onClick={open}><IconMenu2 stroke={3} /></ActionIcon>
       </Group>
@@ -298,8 +315,11 @@ const Footer = () => {
     <Center component="footer" style={footerWrapperStyle}>
       <Group p="md" style={footerContentStyle} justify="flex-end">
         {/* <Text span c="#00adad" style={{marginRight: "auto", fontWeight: 700}}>Jules</Text> */}
-        <Avatar variant="transparent" color="#00adad" radius="0" style={{marginRight: "auto", color: "#00adad"}}>
+        {/* <Avatar variant="transparent" color="#00adad" radius="0" style={{marginRight: "auto", color: "#00adad"}}>
           <IconSquareHalf size="xs" strokeWidth={2} stroke="#00adad" />
+        </Avatar> */}
+        <Avatar variant="transparent" color="#00adad" radius="0" style={{marginRight: "auto", color: "#00adad"}}>
+          <LogoSvg />
         </Avatar>
         <Group>
           {contactInfo.map((item: { [key:string]: string | ReactNode | undefined }, index) => (
